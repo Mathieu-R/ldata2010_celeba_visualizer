d541a9566b978a33c51d41ddfdc9b2caa8c8120a
Mathieu-R <mathieu0709@gmail.com>
2023-12-06 12:26:46 +0100

    add precomputed values

--- precompute_ndarray.py
+++ precompute_ndarray.py
@@ -8,11 +8,14 @@ def precompute_ndarray(filename: str) -> None:
 	df_features = pd.read_parquet(f"data/{filename}__features.gzip")
 	df_embeddings = pd.read_parquet(f"data/{filename}__embeddings.gzip")
 
-	precompute_umap_for_clustering(filename, data_category="features", df=df_features)
-	precompute_umap_for_clustering(filename, data_category="embeddings", df=df_embeddings)
+	# precompute_umap_for_clustering(filename, data_category="features", df=df_features)
+	# precompute_umap_for_clustering(filename, data_category="embeddings", df=df_embeddings)
 
-	precompute_tsne_for_clustering(filename, data_category="features", df=df_features)
-	precompute_tsne_for_clustering(filename, data_category="embeddings", df=df_embeddings)
+	# precompute_tsne_for_clustering(filename, data_category="features", df=df_features)
+	# precompute_tsne_for_clustering(filename, data_category="embeddings", df=df_embeddings)
+
+	precompute_tsne_for_clustering_exaggeration_12(filename, data_category="features", df=df_features)
+	precompute_tsne_for_clustering_exaggeration_12(filename, data_category="embeddings", df=df_embeddings)
 
 def precompute_umap_for_clustering(filename: str, data_category: str, df: pd.DataFrame) -> None:
 	print(f"PRECOMPUTING - filename: {filename}, type: {data_category}, algo: UMAP")
@@ -23,9 +26,16 @@ def precompute_umap_for_clustering(filename: str, data_category: str, df: pd.Dat
 
 def precompute_tsne_for_clustering(filename: str, data_category: str, df: pd.DataFrame) -> None:
 	print(f"PRECOMPUTING - filename: {filename}, type: {data_category}, algo: TSNE")
-	tsne = TSNE(n_components=3, perplexity=50, early_exaggeration=30)
+	tsne = TSNE(n_components=3, perplexity=50, early_exaggeration=30, random_state=42)
 	res = tsne.fit_transform(df)
 	np.save(f"precomputed/{filename}__{data_category}_tsne.npy", res)
+	print("DONE.")
+
+def precompute_tsne_for_clustering_exaggeration_12(filename: str, data_category: str, df: pd.DataFrame) -> None:
+	print(f"PRECOMPUTING - filename: {filename}, type: {data_category}, algo: TSNE, exaggeration: 12")
+	tsne = TSNE(n_components=3, perplexity=50, random_state=42)
+	res = tsne.fit_transform(df)
+	np.save(f"precomputed/{filename}__{data_category}_tsne_exaggeration_12.npy", res)
 	print("DONE.")
 
 precompute_ndarray(filename="celeba_buffalo_l")
